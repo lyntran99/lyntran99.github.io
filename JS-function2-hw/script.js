a4a5efa488810261abc4782ebb41d47024f20aad
lyntran99 <trlinh1810@gmail.com>
2022-05-05 19:42:27 +0700

    update

--- JS-function2-hw/script.js
+++ JS-function2-hw/script.js
@@ -6,9 +6,8 @@
 
 //Bai2
     
-    function stringSliced() {
-        let string1 = "shiDaiShaoNianTuan";
-        return console.log(string1.slice(0,10)+"...");
+    function stringSliced(string1) {
+        return console.log(string1.slice(0,10) + "...");
     }
 
 //Bai3
@@ -18,6 +17,12 @@
         let firstLetter = string2.substring(0,1);
         let theRest = string2.substring(1);
         return console.log(firstLetter.toLocaleUpperCase() + theRest)
+    }
+
+
+    function cutString(num){
+        let string3 = "Thong bao nghi le nhan ngay Thong nhat va Quoc te lao dong 30/4 & 1/5"
+        return console.log(string3.slice(num) + "...")
     }
 
 
