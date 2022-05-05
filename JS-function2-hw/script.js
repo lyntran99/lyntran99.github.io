b3a4663edbc27dc02f147cff7b1544a08f62c8c3
lyntran99 <trlinh1810@gmail.com>
2022-05-05 20:01:35 +0700

    update

--- JS-function2-hw/script.js
+++ JS-function2-hw/script.js
@@ -12,16 +12,14 @@
 
 //Bai3
 
-    function wordUppercase() {
-        let string2 = "techmaster";
+    function wordUppercase(string2) {
         let firstLetter = string2.substring(0,1);
         let theRest = string2.substring(1);
         return console.log(firstLetter.toLocaleUpperCase() + theRest)
     }
 
 
-    function cutString(num){
-        let string3 = "Thong bao nghi le nhan ngay Thong nhat va Quoc te lao dong 30/4 & 1/5"
+    function cutString(string3, num){
         return console.log(string3.slice(num) + "...")
     }
 
